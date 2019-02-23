c3a611ddfa0919efd4e3f914296058d90d8e0556
lex <beyondsnk@163.com>
2019-02-23 21:33:04 +0800

    ch6: extract method(without local variable)

--- src/ch6_RefactoringMethod/WithoutLocalVar.java
+++ src/ch6_RefactoringMethod/WithoutLocalVar.java
@@ -7,10 +7,14 @@ public class WithoutLocalVar {
     void printOwing() {
         List<Integer> owes = Arrays.asList(39, 28, 19, 33, 103);
 
+        printBanner();
+
+        owes.forEach(System.out::println);
+    }
+
+    private void printBanner() {
         System.out.println("***********************");
         System.out.println("**** Customer Owes ****");
         System.out.println("***********************");
-
-        owes.forEach(System.out::println);
     }
 }
