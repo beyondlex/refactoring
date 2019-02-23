8fb1d979d306acce455f72f5ae0ad4d38d7f5540
lex <beyondsnk@163.com>
2019-02-23 15:10:36 +0800

    ch1: Replace type code with state/strategy: Replace Conditional with Polymorphism

--- src/ch1/Price.java
+++ src/ch1/Price.java
@@ -3,27 +3,7 @@ package ch1;
 abstract class Price {
     abstract int getPriceCode();
 
-    public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrenPrice extends Price {
@@ -31,16 +11,37 @@ class ChildrenPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDREN;
     }
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3) {
+            result += (daysRented - 3) * 1.5;
+        }
+        return result;
+    }
 }
 class NewReleasePrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 class RegularPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5;
+        }
+        return result;
     }
 }
