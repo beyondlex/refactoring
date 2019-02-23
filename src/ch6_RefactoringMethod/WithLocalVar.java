82f17e1546fc1f859b9d1309b8b2a6c977f248e0
lex <beyondsnk@163.com>
2019-02-23 21:39:48 +0800

    ch6: extract method(with local variable, but not any reassignment)

--- src/ch6_RefactoringMethod/WithLocalVar.java
+++ src/ch6_RefactoringMethod/WithLocalVar.java
@@ -6,6 +6,15 @@ public class WithLocalVar {
     void printOwing() {
         int localVar = 3;
 
+        printDetails(localVar);
+    }
+
+    /**
+     *
+     * @param localVar 简单地将局部变量作为extracted method的参数
+     */
+    private void printDetails(int localVar) {
+        // 被提炼的代码段：(只是读取局部变量localVar的值，并不修改它)
         System.out.println(localVar);
         System.out.println(_name);
     }
