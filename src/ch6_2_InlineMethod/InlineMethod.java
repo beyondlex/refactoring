d02bf915336569c61ae8bafbc5001612b1fefb2f
lex <beyondsnk@163.com>
2019-02-23 22:03:59 +0800

    InlineMethod: 内部代码 和 函数名称同样清晰易读时，不需要额外的函数

---
+++ src/ch6_2_InlineMethod/InlineMethod.java
@@ -0,0 +1,18 @@
+package ch6_2_InlineMethod;
+
+public class InlineMethod {
+    private int _numberOfLateDeliveries;
+
+    int getRating() {
+        return moreThanFiveLateDeliveries() ? 2 : 1;
+    }
+
+    /**
+     * 内部代码 和 函数名称同样清晰易读
+     * @return boolean
+     */
+    private boolean moreThanFiveLateDeliveries() {
+        // 内部代码
+        return _numberOfLateDeliveries > 5;
+    }
+}
