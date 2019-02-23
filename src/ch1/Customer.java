9a9088e3ca73f04be535f56d10f1c1829ebd2db8
lex <beyondsnk@163.com>
2019-02-23 14:18:50 +0800

    ch1: replace temp with query

--- src/ch1/Customer.java
+++ src/ch1/Customer.java
@@ -25,18 +25,14 @@ public class Customer {
         Enumeration<Rental> rentalEnumeration = rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentalEnumeration.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = rentalEnumeration.nextElement();
-
-            thisAmount = each.getCharge();
-
             frequentRenterPoints ++;
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                     each.getDaysRented() > 1) {
                 frequentRenterPoints ++;
             }
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
+            totalAmount += each.getCharge();
         }
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
