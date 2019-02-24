85acfd82adac14b887a407e68f1832e47b38dec8
lex <beyondsnk@163.com>
2019-02-24 11:13:37 +0800

    MoveMethod

---
+++ src/ch7_MoveMethod/Account.java
@@ -0,0 +1,31 @@
+package ch7_MoveMethod;
+
+public class Account {
+
+    private AccountType _type;
+    private int _daysOverdrawn;
+
+    private double overdraftCharge() {
+        if (_type.isPremium()) {
+            double result = 10;
+            if (_daysOverdrawn > 7) {
+                result += (_daysOverdrawn - 7) * 0.85;
+            }
+            return result;
+        } else {
+            return _daysOverdrawn * 1.75;
+        }
+    }
+
+    double bankCharge() {
+        double result = 4.5;
+        if (_daysOverdrawn > 0) result += overdraftCharge();
+        return result;
+    }
+}
+
+class AccountType {
+    boolean isPremium() {
+        return false;
+    }
+}
