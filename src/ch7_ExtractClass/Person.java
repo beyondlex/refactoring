f86d0e3b272568e3a6de065b3c5316294dda6eb0
lex <beyondsnk@163.com>
2019-02-24 11:27:49 +0800

    ExtractClass

---
+++ src/ch7_ExtractClass/Person.java
@@ -0,0 +1,31 @@
+package ch7_ExtractClass;
+
+public class Person {
+    private String name;
+    private String officeAreaCode;
+    private String officeNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTelephoneNumber() {
+        return "(" + officeAreaCode + ")" + officeNumber;
+    }
+
+    public String getOfficeNumber() {
+        return officeNumber;
+    }
+
+    public void setOfficeNumber(String officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    public String getOfficeAreaCode() {
+        return officeAreaCode;
+    }
+
+    public void setOfficeAreaCode(String officeAreaCode) {
+        this.officeAreaCode = officeAreaCode;
+    }
+}
